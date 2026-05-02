513635817fd5c8fe036d493f1c0746bdfa01d8e4
dlzada <danielluiz1607@gmail.com>
2026-05-02 19:44:54 -0300

    feat: creation TransacaoReponseDTO and what should come out

---
+++ src/main/java/br/com/daniel/dl_wallet/dto/TransacaoResponseDTO.java
@@ -0,0 +1,18 @@
+package br.com.daniel.dl_wallet.dto;
+
+import br.com.daniel.dl_wallet.enums.TipoTransacao;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class TransacaoResponseDTO {
+    private Long id;
+    private String descricao;
+    private BigDecimal valor;
+    private LocalDate data;
+    private TipoTransacao tipo;
+    private String nomeUsuario; // Apenas o nome, sem o objeto inteiro
+}
