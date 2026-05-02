7f4ac1cb6e974b33ff6d9eb1237098b0f6252507
dlzada <danielluiz1607@gmail.com>
2026-05-02 18:50:07 -0300

    feat: creation of dto of Transacao

---
+++ src/main/java/br/com/daniel/dl_wallet/dto/TransacaoRequestDTO.java
@@ -0,0 +1,22 @@
+package br.com.daniel.dl_wallet.dto;
+
+import br.com.daniel.dl_wallet.enums.TipoTransacao;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class TransacaoRequestDTO {
+    @NotNull(message = "A descricao é obrigatoria")
+    private String descricao;
+
+    @NotNull(message = "O valor é obrigatorio")
+    private BigDecimal valor;
+
+    @NotNull(message = "O tipo da transacao é obrigatorio")
+    private TipoTransacao tipoTransacao;
+
+    @NotNull(message = "O id do usuario é obrigatorio")
+    private Long usuarioId;
+}
