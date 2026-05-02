85cf41777bd588735c8a8f6ae146c6c1704c1e3a
dlzada <danielluiz1607@gmail.com>
2026-05-02 18:56:36 -0300

    feat: add method findAllByUsuarioId in TransacaoRepository

--- src/main/java/br/com/daniel/dl_wallet/database/repository/ITransacaoRepository.java
+++ src/main/java/br/com/daniel/dl_wallet/database/repository/ITransacaoRepository.java
@@ -3,5 +3,8 @@ package br.com.daniel.dl_wallet.database.repository;
 import br.com.daniel.dl_wallet.database.model.TransacaoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ITransacaoRepository extends JpaRepository<TransacaoEntity, Long> {
+    List<TransacaoEntity> findAllByUsuarioId(Long usuarioId);
 }
