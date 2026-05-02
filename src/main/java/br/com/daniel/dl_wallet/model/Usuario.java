519c8c8a10c58f2de472725c01d538011b5d1f7c
dlzada <danielluiz1607@gmail.com>
2026-05-02 17:59:45 -0300

    fix: modification strategy of generation od id in Usuario

--- src/main/java/br/com/daniel/dl_wallet/model/Usuario.java
+++ src/main/java/br/com/daniel/dl_wallet/model/Usuario.java
@@ -1,15 +1,23 @@
 package br.com.daniel.dl_wallet.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Table(name = "usuarios")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class Usuario {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
+    @Column(nullable = false)
     String nome;
 
-    @Column(unique = true)
+    @Column(nullable = false, unique = true)
     String email;
 }
