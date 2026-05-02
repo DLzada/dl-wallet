c31471df86602a1b2a02fe087e409d05193d7223
dlzada <danielluiz1607@gmail.com>
2026-05-02 17:38:01 -0300

    feat: startup project

---
+++ src/main/java/br/com/daniel/dl_wallet/DlWalletApplication.java
@@ -0,0 +1,13 @@
+package br.com.daniel.dl_wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/daniel/dl_wallet/DlWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daniel.dl_wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
