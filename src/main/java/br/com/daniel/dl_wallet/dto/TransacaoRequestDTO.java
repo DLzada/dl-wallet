755a7c5bf696e0813c8fd555e0e83e2cde526e73
dlzada <danielluiz1607@gmail.com>
2026-05-03 18:01:14 -0300

    fix: shield of TransacaoRequest not accepting trash or values fools

--- src/main/java/br/com/daniel/dl_wallet/dto/TransacaoRequestDTO.java
+++ src/main/java/br/com/daniel/dl_wallet/dto/TransacaoRequestDTO.java
@@ -3,6 +3,8 @@ package br.com.daniel.dl_wallet.dto;
 import br.com.daniel.dl_wallet.enums.CategoriaTransacao;
 import br.com.daniel.dl_wallet.enums.TipoTransacao;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -10,9 +12,11 @@ import java.math.BigDecimal;
 @Data
 public class TransacaoRequestDTO {
     @NotNull(message = "A descricao é obrigatoria")
+    @Size(min = 3, max = 100, message = "A descrição precisa ter entre 3 e 100 caracteres")
     private String descricao;
 
     @NotNull(message = "O valor é obrigatorio")
+    @Positive(message = "O valor deve ser maior que zero!")
     private BigDecimal valor;
 
     @NotNull(message = "A categoria é obrigatória")
