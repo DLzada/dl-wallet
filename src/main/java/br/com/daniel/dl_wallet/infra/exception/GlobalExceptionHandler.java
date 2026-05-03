1ffdba55eb301eb94279e2513e92fc5285d223c7
dlzada <danielluiz1607@gmail.com>
2026-05-03 18:16:29 -0300

    fix: remove the method handleValidationException in GlobalException, feat: add reponse Statu in method handleValidation

--- src/main/java/br/com/daniel/dl_wallet/infra/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/daniel/dl_wallet/infra/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.ArrayList;
@@ -20,13 +21,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex){
-        String mensagem = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
-        ErrorResponse erro = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public List<ErroCampoDTO> handleValidation(MethodArgumentNotValidException ex){
         List<ErroCampoDTO> erros = new ArrayList<>();
 
