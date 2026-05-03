94862bb10255ce1f8a94c21620e28bd61b0ecd09
dlzada <danielluiz1607@gmail.com>
2026-05-03 08:19:19 -0300

    feat: creation method of GET buscarExtrato in TransacaoController

--- src/main/java/br/com/daniel/dl_wallet/controller/TransacaoController.java
+++ src/main/java/br/com/daniel/dl_wallet/controller/TransacaoController.java
@@ -5,7 +5,11 @@ import br.com.daniel.dl_wallet.dto.TransacaoRequestDTO;
 import br.com.daniel.dl_wallet.service.TransacaoService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDate;
 import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,5 +38,15 @@ public class TransacaoController {
     @ResponseStatus(HttpStatus.OK)
     public BigDecimal buscarSaldo(@PathVariable Long usuarioId){
         return transacaoService.calcularSaldo(usuarioId);
+    }
+
+    @GetMapping("/extrato/{usuarioId}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<TransacaoEntity> buscarExtrato(
+            @PathVariable Long usuarioId,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate inicio,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fim
+            ){
+        return transacaoService.buscarExtratoPorPeriodo(usuarioId, inicio, fim);
     }
 }
