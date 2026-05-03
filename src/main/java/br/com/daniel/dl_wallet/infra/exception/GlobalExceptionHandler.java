1f80f38ddb4a49296b41fce8c9b0ef989172add2
dlzada <danielluiz1607@gmail.com>
2026-05-03 07:55:48 -0300

    feat: creation GlobalExeptionHandler

---
+++ src/main/java/br/com/daniel/dl_wallet/infra/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package br.com.daniel.dl_wallet.infra.exception;
+
+import br.com.daniel.dl_wallet.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException ex){
+        ErrorResponse erro = new ErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex){
+        String mensagem = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        ErrorResponse erro = new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+    }
+}
