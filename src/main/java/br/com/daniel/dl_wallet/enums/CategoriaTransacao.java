99f57a288697042f21ebf5e274e7120c856d8ef5
dlzada <danielluiz1607@gmail.com>
2026-05-03 08:39:11 -0300

    fix: modification structure of enum CategoriaTransacao

--- src/main/java/br/com/daniel/dl_wallet/enums/CategoriaTransacao.java
+++ src/main/java/br/com/daniel/dl_wallet/enums/CategoriaTransacao.java
@@ -1,11 +1,17 @@
 package br.com.daniel.dl_wallet.enums;
 
 public enum CategoriaTransacao {
+
+    //Categoria Entrada
+    SALARIO,
+
+    //Categoria Saida
     ALIMENTACAO,
     LAZER,
     TRANSPORTE,
     SAUDE,
     EDUCACAO,
-    SALARIO,
+
+    //GEral
     OUTROS
 }
