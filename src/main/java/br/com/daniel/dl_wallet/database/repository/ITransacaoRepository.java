c4442d32f1b14737a16c672f2213152e7800cfc1
dlzada <danielluiz1607@gmail.com>
2026-05-03 08:07:45 -0300

    feat: creation method of see between two dates in ITransacaoRepository

--- src/main/java/br/com/daniel/dl_wallet/database/repository/ITransacaoRepository.java
+++ src/main/java/br/com/daniel/dl_wallet/database/repository/ITransacaoRepository.java
@@ -3,8 +3,10 @@ package br.com.daniel.dl_wallet.database.repository;
 import br.com.daniel.dl_wallet.database.model.TransacaoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ITransacaoRepository extends JpaRepository<TransacaoEntity, Long> {
     List<TransacaoEntity> findAllByUsuarioId(Long usuarioId );
+    List<TransacaoEntity> findAllByUsuarioIdAndDataBetween(Long usuarioId, LocalDate dataInicio, LocalDate dataFim);
 }
