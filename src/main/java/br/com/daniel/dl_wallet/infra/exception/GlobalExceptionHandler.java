a642ce71e9cf5a684b70f5852f7c6e3deb18de81
dlzada <danielluiz1607@gmail.com>
2026-05-04 07:43:48 -0300

    feat: creation method handleTypeMismatch in GlobalException

--- src/main/java/br/com/daniel/dl_wallet/infra/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/daniel/dl_wallet/infra/exception/GlobalExceptionHandler.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -28,7 +31,15 @@ public class GlobalExceptionHandler {
         ex.getBindingResult().getFieldErrors().forEach(error -> {
             erros.add(new ErroCampoDTO(error.getField(), error.getDefaultMessage()));
         });
-
         return erros;
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleTypeMismatch(MethodArgumentTypeMismatchException ex){
+        Map<String, String> erro = new HashMap<>();
+        erro.put("mensagem", String.format("O parametro '%s' recebeu o valor '%s' que é inválido. Esperado: %s", ex.getName(), ex.getValue(), ex.getRequiredType().getSimpleName()));
+
+        return erro;
     }
 }
